1231230a138b679886838914bf5dc0d4802f68df
Lucas Ferreira <lokoporgame194@gmail.com>
2025-11-26 21:27:56 -0300

    models criado

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Mensagem(models.Model):
+    sala = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    # Por enquanto, vou salvar o nome como texto para simplificar
+    autor = models.CharField(max_length=255, default="Anônimo")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.autor}: {self.conteudo} ({self.sala})"
