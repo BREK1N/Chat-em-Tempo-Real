7a8bd10fd3280cfa56815af5544e546d20847d57
Lucas Ferreira <lokoporgame194@gmail.com>
2025-11-26 21:15:04 -0300

    Roteamento WebSocket

--- src/setup/asgi.py
+++ src/setup/asgi.py
@@ -18,4 +18,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
 })
